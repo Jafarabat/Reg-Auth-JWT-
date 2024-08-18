f6e5d8e96c678573a12f4f2c362956de619841a9
Jafarabat <137901795+Jafarabat@users.noreply.github.com>
2024-08-18 22:07:34 +0300

    Delete Services directory

--- Services/IAuthService.cs
+++
@@ -1,9 +0,0 @@
-using System.Security.Claims;
-
-public interface IAuthService
-{
-    string GenerateJwtToken(User user);
-    string GenerateRefreshToken();
-    ClaimsPrincipal GetPrincipalFromExpiredToken(string token, IConfiguration config);
-    Task UpdateRefreshToken(User user);
-}
